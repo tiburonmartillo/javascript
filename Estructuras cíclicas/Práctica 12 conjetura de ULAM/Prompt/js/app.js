fd3624c25921800036d9e53e207475e8a5e56176
tiburonmartillo <sivillanueva@outlook.com>
2020-03-03 11:01:09 -0600

    práctica con la serie de ULAM cuando se ingresa un número

---
+++ Estructuras cíclicas/Práctica 12 conjetura de ULAM/Prompt/js/app.js
@@ -0,0 +1,27 @@
+eventListener();
+
+function eventListener(){
+    const button = document.getElementById('btnInit');
+    
+    button.addEventListener('click',iniciarApp);
+}
+function iniciarApp(){
+    let numero = 0;
+    numero = Number(prompt('Ingrese un número'));
+    const listGroup = document.getElementById('listaULAM');
+    
+    while(numero!=1){
+        if (numero%2==0){
+            numero/=2
+        }else {numero=(numero*3)+1}
+        
+        console.log(numero);
+        
+        const li = document.createElement('li');
+        li.setAttribute('class','list-group-item');
+        li.innerText=`${numero}`;
+        listGroup.appendChild(li);
+    }
+
+        
+    }
