5136043be09307119618010cec3d1756f61df297
tiburonmartillo <sivillanueva@outlook.com>
2020-03-09 10:37:06 -0600

    don mandilon

--- Estructuras cíclicas/Práctica 19 El mandilón/js/app.js
+++ Estructuras cíclicas/Práctica 19 El mandilón/js/app.js
@@ -5,33 +5,51 @@ function eventListener(){
 
 }
 let acumuladorVentas=0;
-let acumuladorciudades=0;
+let acumuladorCiudades=0;
 let acumuladorSucursales=0;
-let acumuladorempleados=0;
+let acumuladorEmpleados=0;
+
 function iniciarApp(){
     let ciudades= Number(prompt('Ingrese número de ciudades'));
-    acumuladorciudades+=ciudades;
-
+    const listaCiudades=document.getElementById('listaCiudades');
+    const listaSucursales=document.getElementById('listaSucursales');
+    const listaEmpleados=document.getElementById('listaEmpleados');
     for(let i=1;i<=ciudades;i++){
-        let sucursales= Number(prompt(`ingrese elnúmero de sucursales en la ciudad ${i}`));
-        acumuladorSucursales+=sucursales;
+        let sucursales= Number(prompt(`Ingrese el número de sucursales en la ciudad ${i}`));
+        acumuladorCiudades+=ciudades
+        
         for(let e=1;e<=sucursales;e++){
             let empleados=Number(prompt(`ingrese el número de empleados de la tienda ${e}`));
-            acumuladorempleados+=empleados;
+            acumuladorSucursales+=sucursales
+            
             for(let v=1;v<=empleados;v++){
                 let ventas=Number(prompt(`Ingrese la venta del empleado ${v}`));
-                console.log(`las ventas del empleado ${v} en la sucursal ${e} en la ciudad ${i} son de ${ventas} `);
-                
                 acumuladorVentas+=ventas;
-                console.log(ventas);
+                acumuladorEmpleados+=empleados
+                
+                const li = document.createElement('li');
+                    li.setAttribute('class', 'list-group-item list-group-item-danger','col');
+                    li.innerText = `La venta del vendedor ${acumuladorEmpleados/empleados} de la sucursal ${acumuladorSucursales/sucursales} de la ciudad ${acumuladorCiudades/ciudades} fueron de ${ventas}`;
+                    listaEmpleados.appendChild(li);
+
+                    console.log(ciudades,sucursales,empleados,ventas);
+                    
+                }
+                
+                
+                const li = document.createElement('li');
+                li.setAttribute('class', 'list-group-item list-group-item-info','col');
+                li.innerText = `La venta de la ciudad ${acumuladorCiudades/sucursales} es de ${acumuladorVentas/empleados}`;
+                listaCiudades.appendChild(li);
             }
+            const li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item list-group-item-warning','col');
+            li.innerText = `La venta de la sucursal ${acumuladorSucursales/2} de la ciudad ${acumuladorCiudades/2} es de ${acumuladorVentas}`;
+            listaSucursales.appendChild(li);
             
-        }
-    }
-    console.log(acumuladorciudades);
-    console.log(acumuladorSucursales);
-    console.log(acumuladorempleados);
-    
-    console.log(acumuladorVentas);//venta total de la cadena
         
     }
+    
+    
+}
+
